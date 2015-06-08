6d98f77fe467e3802b5653095eedd12777c4510b
Jacob Peterson <tranquilwinter7@gmail.com>
2015-06-08 16:13:34 -0600

    set up basic backend structure

---
+++ server.js
@@ -0,0 +1,34 @@
+// Dependencies
+var express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var mongoose = require('mongoose');
+var request = require('request');
+
+// Express
+var app = express();
+
+// Middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+// Endpoints
+request('https://api.coindesk.com/v1/bpi/currentprice.json', function (error, response, body) {
+  	if (!error && response.statusCode == 200) {
+    	console.log(body)
+  	}
+});
+
+// Connections
+var port = 8081
+
+var mongoUri = 'mongodb://localhost:27017/bitcurve';
+
+mongoose.connect(mongoUri);
+mongoose.connection.once('open', function(err, db) {
+	if(!err) {
+    	console.log('Connected to MongoDB at ', mongoUri);
+  	}  
+});
+
+app.listen(port);
