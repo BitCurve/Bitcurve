f24e531b0fa847d2f232cf7ce9335d32a3dd7b37
Whitney <whitneywooyy@gmail.com>
2015-06-15 17:18:06 -0600

    More artDashboard Ctrl/Service

--- public/js/controllers/artDashboardCtrl.js
+++ public/js/controllers/artDashboardCtrl.js
@@ -3,9 +3,9 @@
 	angular.module('bitcurve')
 		.controller('artDashboardCtrl', ['$scope','artDashboardService', function($scope, artDashboardService){
 
-			// $scope.$on('reportChange', function(event, data){
-			// 	$scope.changeDisplay([data])
-			// });
+			$scope.$on('dataChange', function(event, data){
+				$scope.changeDisplay([data])
+			});
 	//2
 			$scope.getData = function() {
 				artDashboardService.getData().then(function(response) {
@@ -17,55 +17,92 @@
 						//volume = []
 						//transactionFees = []
 					// console.log("ctrl response", response);
-					$scope.averageNumberOfTransactionsPerBlock = [];
-					$scope.dailyMinersRevenue = [];
-					$scope.date = [];
-					$scope.day = [];
-					$scope.difficulty = [];
-					$scope.month = [];
-					$scope.numberofUniqueBitcoinAddresses = [];
-					$scope.price = [];
-					$scope.totalCirculation = [];
-					$scope.totalOutputVolumeValue = [];
-					$scope.totalTransactionFees = [];
-					$scope.year = [];
-					$scope.id = [];
+					var averageNumberOfTransactionsPerBlock = [];
+					var dailyMinersRevenue = [];
+					var date = [];
+					var day = [];
+					var difficulty = [];
+					var month = [];
+					var numberofUniqueBitcoinAddresses = [];
+					var price = [];
+					var totalCirculation = [];
+					var totalOutputVolumeValue = [];
+					var totalTransactionFees = [];
+					var year = [];
+					var id = [];
 
 					response.map(function(res){
-						$scope.averageNumberOfTransactionsPerBlock.push(res.averageNumberOfTransactionsPerBlock);
-						$scope.dailyMinersRevenue.push(res.dailyMinersRevenue);
-						$scope.date.push(res.date);
-						$scope.day.push(res.day);
-						$scope.difficulty.push(res.difficulty);
-						$scope.month.push(res.month);
-						$scope.numberofUniqueBitcoinAddresses.push(res.numberofUniqueBitcoinAddresses);
-						$scope.price.push(res.price);
-						$scope.totalCirculation.push(res.totalCirculation);
-						$scope.totalOutputVolumeValue.push(res.totalOutputVolumeValue);
-						$scope.totalTransactionFees.push(res.totalTransactionFees);
-						$scope.year.push(res.year);
-						$scope.id.push(res.id);
+						averageNumberOfTransactionsPerBlock.push(res.averageNumberOfTransactionsPerBlock);
+						dailyMinersRevenue.push(res.dailyMinersRevenue);
+						date.push(res.date);
+						day.push(res.day);
+						difficulty.push(res.difficulty);
+						month.push(res.month);
+						numberofUniqueBitcoinAddresses.push(res.numberofUniqueBitcoinAddresses);
+						price.push(res.price);
+						totalCirculation.push(res.totalCirculation);
+						totalOutputVolumeValue.push(res.totalOutputVolumeValue);
+						totalTransactionFees.push(res.totalTransactionFees);
+						year.push(res.year);
+						id.push(res.id);
 					});
-					console.log('$scope.averageNumberOfTransactionsPerBlock', $scope.averageNumberOfTransactionsPerBlock);
-					console.log('$scope.dailyMinersRevenue', $scope.dailyMinersRevenue);
-					console.log('$scope.date', $scope.date);
-					console.log('$scope.day', $scope.day);
-					console.log('$scope.difficulty', $scope.difficulty);
-					console.log('$scope.month', $scope.month);
-					console.log('$scope.numberofUniqueBitcoinAddresses', $scope.numberofUniqueBitcoinAddresses);
-					console.log('$scope.price', $scope.price);
-					console.log('$scope.totalCirculation', $scope.totalCirculation);
-					console.log('$scope.totalOutputVolumeValue', $scope.totalOutputVolumeValue);
-					console.log('$scope.totalTransactionFees', $scope.totalTransactionFees);
-					console.log('$scope.year', $scope.year);
-					console.log('$scope.id', $scope.id);
+					// console.log('averageNumberOfTransactionsPerBlock', averageNumberOfTransactionsPerBlock);
+					// console.log('dailyMinersRevenue', dailyMinersRevenue);
+					// console.log('date', date);
+					// console.log('day', day);
+					// console.log('difficulty', difficulty);
+					// console.log('month', month);
+					// console.log('numberofUniqueBitcoinAddresses', numberofUniqueBitcoinAddresses);
+					// console.log('price', price);
+					// console.log('totalCirculation', totalCirculation);
+					// console.log('totalOutputVolumeValue', totalOutputVolumeValue);
+					// console.log('totalTransactionFees', totalTransactionFees);
+					// console.log('year', year);
+					// console.log('id', id);
 				});
 				
 				//based on the radio selection, the corresponding data type will populate $scope.finalData
 			};
 			$scope.getData();
 
-			//$scope.finalData = ....
+			$scope.finalData = function(dataSelection){
+				// console.log("dataSelection", dataSelection);
+				var userSelect = undefined;
+				for (var key in dataSelection) {
+					userSelect = dataSelection[key];
+				}
+				console.log("userSelect", userSelect);
+				switch(userSelect) {
+					case "averageNumberOfTransactionsPerBlock" :
+						console.log("hey averageNumberOfTransactionsPerBlock");
+						// Send averageNumberOfTransactionsPerBlock (array) to d3ArtDashboardDirective.js
+						break;
+					case "dailyMinersRevenue" :
+						console.log("hey dailyMinersRevenue");
+						break;
+					case "difficulty" :
+						console.log("hey difficulty");
+						break;
+					case "numberofUniqueBitcoinAddresses" :
+						console.log("hey numberofUniqueBitcoinAddressesy");
+						break;
+					case "price" :
+						console.log("hey price");
+						break;
+					case "totalCirculation" :
+						console.log("hey totalCirculation");
+						break;
+					case "totalOutputVolumeValue" :
+						console.log("hey totalOutputVolumeValue");
+						break;
+					case "totalTransactionFees" :
+						console.log("hey totalTransactionFees");
+						break;
+					default :
+						console.log("shoot me now");
+				}
+
+			}	// End $scope.finalData
 
 		}])//end controller
 
