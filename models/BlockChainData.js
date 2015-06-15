67db545ce1c90897c72733fe213b77a0b79999c7
Jacob Peterson <tranquilwinter7@gmail.com>
2015-06-15 13:25:27 -0600

    started working on login

--- models/BlockChainData.js
+++ models/BlockChainData.js
@@ -1,15 +1,29 @@
 var mongoose = require("mongoose");
 
 var blockChainSchema = new mongoose.Schema({
-	Month: {type: Number},
-	Day: {type: Number},
-    Year: {type: Number},
-    Price: {type: Number},
+	date: {type: String},
+	month: {type: Number},
+    day: {type: Number},
+    year: {type: Number},
+    price: {type: Number},
     totalCirculation: {type: Number},
+    hashRate: {type: Number},
     totalTransactionFees: {type: Number},
-    numberOfUniqueBitcoinAddressesUsed: {type: Number},
-    totalOutputVolumeValue: {type: Number},
-    averageNumberOfTransactionsPerBlock: {type: Number}
+    numberBitcoinMined: {type: Number},
+    numberTx: {type: Number},
+    numberBlocksMined: {type: Number},
+    minutesBetweenBlocks: {type: Number},
+    numberBlocksTotal: {type: Number},
+    tradeVolumeUSD: {type: Number},
+    transactionVolume: {type: Number},
+    blockSize: {type: Number},
+    nextRetarget: {type: Number},
+    tradeVolumeBTC: {type: Number},
+    estimatedBTCSent: {type: Number},
+    totalBTCSent: {type: Number},
+    dailyMinersRevenueUSD: {type: Number},
+    dailyMinersRevenueBTC: {type: Number},
+    difficulty: {type: Number},
 });
 
 module.exports = mongoose.model('BlockChainData', blockChainSchema);
